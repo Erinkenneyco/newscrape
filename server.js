a5f563e954ebdc4b01a9ba30ef53eae065ab70e7
erdakjo <ekenney60@gmail.com>
2019-02-25 18:06:41 -0700

    adding new scraping functionality

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var express = require("express");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
+
+
+
 // Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
 
@@ -15,13 +18,15 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Require our routes
+
 var routes = require("./routes");
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("/public"));
+app.use(express.static("public"));
+
 //app.use(express.static(__dirname + 'public'));
 
 // Connect Handlebars to our Express app
@@ -30,6 +35,8 @@ app.set("view engine", "handlebars");
 
 // Have every request go through our route middleware
 app.use(routes);
+
+
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
